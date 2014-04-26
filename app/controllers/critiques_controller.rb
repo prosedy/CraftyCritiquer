806e73a3e407b159651c466aa0fdf92f7a1f955e
Matthew Bunday <zenceph@gmail.com>
2014-04-26 19:55:09 -0400

    Setup basics for critique.

--- app/controllers/critiques_controller.rb
+++ app/controllers/critiques_controller.rb
@@ -1,2 +1,28 @@
 class CritiquesController < ApplicationController
+  def index
+    
+  end
+
+  def new
+    @critique = Critique.new
+  end
+
+  def show
+    @critique = Critique.find(params[:id])
+  end
+
+  def create
+    @critique = Critique.new(critique_params)
+
+    if @critique.save
+      redirect_to @critique
+    else
+      render 'new'
+    end
+  end
+
+private
+  def critique_params
+    params.require(:critique).permit(:body)
+  end
 end
