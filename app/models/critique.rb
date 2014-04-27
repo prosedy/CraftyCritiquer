69c29cb1a4ae1945b92588700da006661eed2e9c
Matthew Bunday <zenceph@gmail.com>
2014-04-26 20:07:26 -0400

    Add PertinentParser and TactfulTokenizer.

--- app/models/critique.rb
+++ app/models/critique.rb
@@ -1,2 +1,12 @@
 class Critique < ActiveRecord::Base
+  before_save :tokenize_html
+
+  def tokenize_html
+    h = PertinentParser::html(self[:body])
+    sentences = Tokenizer::tokenizer.tokenize_text(h)
+    sentences.each_with_index do |sentence, i|
+      h.wrap_out("<span class='sentence' id='#{i+1}'>", sentence)
+    end
+    self[:tokenized] = h.apply
+  end
 end
